d349481ad566aa7932463d5b5793244d9546cca9
Nyazabe <snyazabe@gmail.com>
2025-08-12 11:45:42 +0900

    Initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtrVohm-AKdn6YSye0kWCEmL_2K4JFeFE",
+  authDomain: "socraticgenai-nyaz.firebaseapp.com",
+  projectId: "socraticgenai-nyaz",
+  storageBucket: "socraticgenai-nyaz.firebasestorage.app",
+  messagingSenderId: "553593458694",
+  appId: "1:553593458694:web:74d1b610c0486599043387",
+  measurementId: "G-KY2KMS511V"
+};
